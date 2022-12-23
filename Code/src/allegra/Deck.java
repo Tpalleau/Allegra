3cd56020ca28b76d1539c24168dcb5f04b685628
Tom <tom.palleau@gmail.com>
2022-12-23 22:46:16 +0100

    constructer builds the deck and shuffles

--- Code/src/allegra/Deck.java
+++ Code/src/allegra/Deck.java
@@ -1,10 +1,11 @@
 package allegra;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class Deck {
-	protected List<Card> cards;
+	protected List<Card> cards = new ArrayList<Card>();
 	
 	public Card pickCard()
 	{		
@@ -16,36 +17,25 @@ public class Deck {
 		return pickedCard;
 	}
 	
-	//FIX THE ISSUE WITH NULL AND INITIALISATION
-	public void shuffle()
+	private void fillDeck()
 	{
-		final List<Card> new_deck = null;
-		final Random rand = new Random();
-		while (!this.cards.isEmpty())
-		{
-			final int rand_num = rand.nextInt(this.cards.size());
-			new_deck.add(this.cards.get(rand_num));
-			this.cards.remove(rand_num);
-		}
-		this.cards.addAll(new_deck);
-	}
-	
-	public void fillDeck()
-	{
-		final List<Card> temp = null;
 		for (int i=0; i<8; i++)
 			for (int j=-1; j<4; j++)
-				temp.add(new Card(j));
+				cards.add(new Card(j));
 		
 		for (int i=0; i<11; i++)
 			for (int j=4; j<8; j++)
-				temp.add(new Card(j));
+				cards.add(new Card(j));
 		
 		for (int i=0; i<8; i++)
 			for (int j=8; j<12; j++)
-				temp.add(new Card(j));
+				cards.add(new Card(j));
 		
-		this.cards.clear();
-		this.cards.addAll(temp);
+	}
+
+	public Deck(){
+		fillDeck();
+		Collections.shuffle(this.cards);
+		System.out.println(cards.size());
 	}
 }

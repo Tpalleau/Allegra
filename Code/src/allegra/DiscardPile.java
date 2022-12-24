8d3279890118ccef247ffec159661ecf6fc048e6
Tom <tom.palleau@gmail.com>
2022-12-24 16:53:54 +0100

    discard filters null cards, update of show matrix to all and playerplaying matrix

--- Code/src/allegra/DiscardPile.java
+++ Code/src/allegra/DiscardPile.java
@@ -5,9 +5,12 @@ public class DiscardPile extends Pile {
 	 * @brief card sent to top of pile (top pile is index 0)
 	 * @param c The card that the player discards
 	 */
-	public void discardCard(Card c)
+	public void discardCard(Card card)
 	{
-		this.cards.add(0, c);
+		// evoids having empty cards in list
+		if (card != null){
+			this.cards.add(0, card);
+		}
 	}
 	
 	/**

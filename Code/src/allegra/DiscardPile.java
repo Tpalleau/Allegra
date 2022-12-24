e1303e951a756cb7f1a0f6d8ff6f98b846c3e815
Tom <tom.palleau@gmail.com>
2022-12-24 11:13:21 +0100

    modified Deck(DrawPile + Pile) to 2 seperate classes

---
+++ Code/src/allegra/DiscardPile.java
@@ -0,0 +1,21 @@
+package allegra;
+
+public class DiscardPile extends Pile {
+	/**
+	 * @brief This method allows players to discard their card and add it on top of the discard pile
+	 * @param c The card that the player discards
+	 */
+	public void discardCard(Card c)
+	{
+		this.cards.add(0, c);
+	}
+	
+	/**
+	 * @brief This method gives us the value of the top card in the discard pile
+	 * @return The value of the top card
+	 */
+	public int showTopCard()
+	{
+		return this.cards.get(0).getValue();
+	}
+}
